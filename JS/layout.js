599200446b5aa94b35bc59dc8371b94d85065b56
Ronak Patel <ronak747@Ronaks-MacBook-Pro.local>
2025-05-22 17:12:01 -0400

    fixed ContactUs.html to have proper scripting for layout folder

--- JS/layout.js
+++
@@ -1,8 +0,0 @@
-function loadComponent(id, file) {
-    fetch(file)
-      .then(res => res.text())
-      .then(data => document.getElementById(id).innerHTML = data);
-  }
-  
-  loadComponent('TopBar', 'TopBar.html');
-  loadComponent('SideBar', 'SideBar.html');
