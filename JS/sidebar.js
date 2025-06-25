62e7081d35ef2b0abfc6beb033ce92182a076836
Ronak Patel <ronak747@n3-96-231.dhcp.drexel.edu>
2025-06-25 16:32:20 -0400

    sidebar update

---
+++ JS/sidebar.js
@@ -0,0 +1,15 @@
+
+  const menuToggle = document.getElementById('menu-toggle');
+  const sidebar = document.getElementById('sidebar');
+  const sidebarLinks = document.querySelectorAll('#sidebar a');
+
+  menuToggle.addEventListener('click', () => {
+    sidebar.classList.toggle('active');
+  });
+
+  sidebarLinks.forEach(link => {
+    link.addEventListener('click', () => {
+      sidebar.classList.remove('active');
+    });
+  });
+
