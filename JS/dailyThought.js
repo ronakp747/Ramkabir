413ce2a451395168b24f564ff868b738e0cf12f1
Ronak Patel <ronak747@Mac.lan>
2025-08-16 15:19:38 -0400

    test daily thought

--- JS/dailyThought.js
+++ JS/dailyThought.js
@@ -1,11 +1,15 @@
+// const dailyQuotes = [
+  //  { quote: "The only way to do great work is to love what you do.", author: "Steve Jobs" },
+  //  { quote: "Life is what happens when you're busy making other plans.", author: "John Lennon" },
+  //  { quote: "Be yourself; everyone else is already taken.", author: "Oscar Wilde" },
+   // { quote: "Do not watch the clock. Do what it does. Keep going.", author: "Sam Levenson" },
+   // { quote: "Success is not final, failure is not fatal: It is the courage to continue that counts.", author: "Winston Churchill" },
+   // { quote: "The greatest wealth is to live content with little.", author: "Plato" }
+  // ];
 const dailyQuotes = [
-    { quote: "The only way to do great work is to love what you do.", author: "Steve Jobs" },
-    { quote: "Life is what happens when you're busy making other plans.", author: "John Lennon" },
-    { quote: "Be yourself; everyone else is already taken.", author: "Oscar Wilde" },
-    { quote: "Do not watch the clock. Do what it does. Keep going.", author: "Sam Levenson" },
-    { quote: "Success is not final, failure is not fatal: It is the courage to continue that counts.", author: "Winston Churchill" },
-    { quote: "The greatest wealth is to live content with little.", author: "Plato" }
-  ];
+  { quote: "सद्गुरु संदेशों मोकले  कचा बचा सब आव नाम चिंतामणि जीवणा जो भावे सौ पाव", author: "जीवणजी महाराज" }
+
+];
   
   function updateDailyThought() {
     const now = new Date();
